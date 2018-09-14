aa831ad7a2590f09532f77cfafec10f16595a358
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-09-13 21:38:37 -0300

    delata pasta

--- lastfm/last.fm_scraper/__init__.py
+++
@@ -1,2 +0,0 @@
-import BeautifulSoup
-import requests
--- lastfm/last.fm_scraper/auth.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-class LastfmScraperAuth(object):
-	
-	def __init__(self): 
-		self.api_key = "put the api key"
-		self.shared_secret = "put the shared secret"

49191b156f55085811a30ff26c2de1c6fa587021
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2018-09-17 22:49:11 -0300

    adiciona outros métodos para artista

---
+++ lastfm/requests/artist.py
@@ -0,0 +1,42 @@
+__author__ = "Ivyna Santino"
+
+import requests
+
+#importar do auth.py
+URL = ""
+API_KEY = ""
+
+def url(method, search_name):
+	return URL + method + search_name + "&api_key=" + API_KEY + "&format=json"
+
+def get_top_tag_artist(disco):
+	url_request = url("tag.gettopartists&tag=", disco)
+	return requests.get(url_request).content
+	
+def get_top_album_artist(artist):
+	url_request = url("artist.gettopalbums&artist=", artist)
+	return request.get(url_request).content
+	
+def get_getCorrection_artist(artist):
+	url_request = url("artist.getcorrection&artist=", artist)
+	return request.get(url_request).content
+
+def get_getInfo_artist(artist):
+	url_request = url("artist.getinfo&artist=", artist)
+	return request.get(url_request).content
+	
+def get_getSimilar_artist(artist):
+	url_request = url("artist.getsimilar&artist=", artist)
+	return request.get(url_request).content
+	
+def get_getTags_artist(artist):
+	url_request = url("artist.getTags&artist=", artist)
+	return request.get(url_request).content
+
+def get_getInfo_artist(artist):
+	url_request = url("artist.getinfo&artist=", artist)
+	return request.get(url_request).content
+
+def get_top_tracks_artist(artist):
+	url_request = url("artist.gettoptracks&artist=", artist)
+	return request.get(url_request).content
